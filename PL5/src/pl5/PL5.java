80aea61d45ff5deb3a42d173aa295bdc938f502b
DelmarMG <delmarmg91@gmail.com>
2013-03-12 17:35:48 +0000

    Teve de ser

---
+++ PL5/src/pl5/PL5.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl5;
+
+/**
+ *
+ * @author Delmar
+ */
+public class PL5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("O hugo é gay!");
+    }
+}
