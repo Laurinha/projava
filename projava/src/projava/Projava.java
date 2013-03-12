5ba0e33b5931179d39f816eeaffc3877071e333e
i100623 <i100623@PCI080.dei.isep.ipp.pt>
2013-03-12 17:35:57 +0000

    criação do projecto java

---
+++ projava/src/projava/Projava.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projava;
+
+/**
+ *
+ * @author i100623
+ */
+public class Projava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OLA");
+    }
+}
